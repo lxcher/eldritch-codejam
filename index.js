e532db4d53011c33f7ff383fa8830f09d668bc19
lxcher <aleksejcerenkevic@gmail.com>
2022-08-26 15:38:18 +0300

    fix: correct tracker

--- index.js
+++ index.js
@@ -1,29 +1,81 @@
 import blueCardsData from "./data/mythicCards/blue/index.js";
 import brownCardsData from "./data/mythicCards/brown/index.js";
 import greenCardsData from "./data/mythicCards/green/index.js";
+import ancientsData from "./data/ancients.js";
 
 const shuffleCardsBtn = document.querySelector('.button_shuffle')
 const deck = document.querySelector('.deck')
 const tracker = document.querySelector('.tracker')
+const ancients = document.querySelector('.ancients')
+
+ancients.addEventListener('click', chooseAncient)
+
+let greenCardsQty = 0
+let brownCardsQty = 0
+let blueCardsQty = 0
+
+let ancientObj = {}
+
+function getSumOfCards() {
+    greenCardsQty = +ancientObj.firstStage.greenCards + +ancientObj.secondStage.greenCards + +ancientObj.thirdStage.greenCards
+    brownCardsQty = +ancientObj.firstStage.brownCards + +ancientObj.secondStage.brownCards + +ancientObj.thirdStage.brownCards
+    blueCardsQty = +ancientObj.firstStage.blueCards + +ancientObj.secondStage.blueCards + +ancientObj.thirdStage.blueCards
+}
+
+function clearEverything() {
+    ancientObj = {}
+    greenCardsQty = 0
+    brownCardsQty = 0
+    blueCardsQty = 0
+    index = 0
+    firstStageSum = 0
+    secondStageSum = 0
+    thirdStageSum = 0
+    blueCards = []
+    brownCards = []
+    greenCards = []
+    stageOne = []
+    stageTwo = []
+    stageThree = []
+    result = []
+    deck.style.backgroundImage = `url('./assets/mythicCardBackground.png')`
+}
+
+function chooseAncient(e) {
+    clearEverything()
+    let ancient = e.target
+    if (ancient.classList.contains('ancients__ancient_azathoth')) {
+        ancientObj = ancientsData[0]
+    }
+    if (ancient.classList.contains('ancients__ancient_cthulthu')) {
+        ancientObj = ancientsData[1]
+    }
+    if (ancient.classList.contains('ancients__ancient_iogsothot')) {
+        ancientObj = ancientsData[2]
+    }
+    if (ancient.classList.contains('ancients__ancient_shubniggurath')) {     
+        ancientObj = ancientsData[3]
+    }
+    getSumOfCards()
+    setTrackerValues()
+    setStagesSums()
+}
 
 const stageOneTracker = tracker.children[0].lastElementChild.children
 const stageTwoTracker = tracker.children[1].lastElementChild.children
 const stageThreeTracker = tracker.children[2].lastElementChild.children
 
 function setTrackerValues() {
-    stageOneTracker[0].textContent = '1'
-    stageOneTracker[1].textContent = '2'
-    stageOneTracker[2].textContent = '1'
-    stageTwoTracker[0].textContent = '2'
-    stageTwoTracker[1].textContent = '3'
-    stageTwoTracker[2].textContent = '1'
-    stageThreeTracker[0].textContent = '2'
-    stageThreeTracker[1].textContent = '4'
-    stageThreeTracker[2].textContent = '0'
+    stageOneTracker[0].textContent = ancientObj.firstStage.greenCards
+    stageOneTracker[1].textContent = ancientObj.firstStage.brownCards
+    stageOneTracker[2].textContent = ancientObj.firstStage.blueCards
+    stageTwoTracker[0].textContent = ancientObj.secondStage.greenCards
+    stageTwoTracker[1].textContent = ancientObj.secondStage.brownCards
+    stageTwoTracker[2].textContent = ancientObj.secondStage.blueCards
+    stageThreeTracker[0].textContent = ancientObj.thirdStage.greenCards
+    stageThreeTracker[1].textContent = ancientObj.thirdStage.brownCards
+    stageThreeTracker[2].textContent = ancientObj.thirdStage.blueCards
 }
-
-window.addEventListener('load', setTrackerValues())
-
 
 let index = 0
 
@@ -38,16 +90,28 @@ function decreaseTrackerValue(color, stage, i) {
     }
 }
 
+let firstStageSum = 0
+let secondStageSum = 0
+let thirdStageSum = 0
+
+function setStagesSums() {
+    firstStageSum = +ancientObj.firstStage.greenCards + +ancientObj.firstStage.brownCards + +ancientObj.firstStage.blueCards
+    secondStageSum = +ancientObj.secondStage.greenCards + +ancientObj.secondStage.brownCards + +ancientObj.secondStage.blueCards
+    thirdStageSum = +ancientObj.thirdStage.greenCards + +ancientObj.thirdStage.brownCards + +ancientObj.thirdStage.blueCards
+}
+
 function changeTrackerValues() {
-    if (index < 4) {
+    if (index < firstStageSum) {
         decreaseTrackerValue('green', stageOneTracker, 0)
         decreaseTrackerValue('brown', stageOneTracker, 1)
         decreaseTrackerValue('blue', stageOneTracker, 2)
-    } else if (index < 10 && index >= 4) {
+    }
+    if (index < secondStageSum + firstStageSum && index >= firstStageSum) {
         decreaseTrackerValue('green', stageTwoTracker, 0)
         decreaseTrackerValue('brown', stageTwoTracker, 1)
         decreaseTrackerValue('blue', stageTwoTracker, 2)
-    } else {
+    }
+    if (index >= secondStageSum + firstStageSum) {
         decreaseTrackerValue('green', stageThreeTracker, 0)
         decreaseTrackerValue('brown', stageThreeTracker, 1)
         decreaseTrackerValue('blue', stageThreeTracker, 2)
@@ -77,9 +141,9 @@ shuffleCardsBtn.addEventListener('click', () => {
 })
 
 function shuffleCards() {
-    setCards(2, blueCards, blueCardsData)
-    setCards(9, brownCards, brownCardsData)
-    setCards(5, greenCards, greenCardsData)
+    setCards(blueCardsQty, blueCards, blueCardsData)
+    setCards(brownCardsQty, brownCards, brownCardsData)
+    setCards(greenCardsQty, greenCards, greenCardsData)
 }
 
 function setCards(quantity, deck, source) {
@@ -99,22 +163,21 @@ let stageThree = []
 
 
 function setStages() {
-    setStage(stageOne, 1, greenCards)
-    setStage(stageOne, 2, brownCards)
-    setStage(stageOne, 1, blueCards)
-    setStage(stageTwo, 2, greenCards)
-    setStage(stageTwo, 3, brownCards)
-    setStage(stageTwo, 1, blueCards)
-    setStage(stageThree, 2, greenCards)
-    setStage(stageThree, 4, brownCards)
-    setStage(stageThree, 0, blueCards)
+    setStage(stageOne, ancientObj.firstStage.greenCards, greenCards)
+    setStage(stageOne, ancientObj.firstStage.brownCards, brownCards)
+    setStage(stageOne, ancientObj.firstStage.blueCards, blueCards)
+    setStage(stageTwo, ancientObj.secondStage.greenCards, greenCards)
+    setStage(stageTwo, ancientObj.secondStage.brownCards, brownCards)
+    setStage(stageTwo, ancientObj.secondStage.blueCards, blueCards)
+    setStage(stageThree, ancientObj.thirdStage.greenCards, greenCards)
+    setStage(stageThree, ancientObj.thirdStage.brownCards, brownCards)
+    setStage(stageThree, ancientObj.thirdStage.blueCards, blueCards)
 }
 
 function setStage(stage, quantity, source) {
     for (let i = 0; i < quantity; i++) {
         let item = source.shift()
         stage.push(item)
-        
     }
 }
 
